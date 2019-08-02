4c2fd0a9f9bdce4ca5266078fc01dc58406df780
John-John Tedro <udoprog@tedro.se>
2019-08-02 11:20:04 +0200

    Release 1.0.0

---
+++ README.md
@@ -0,0 +1,14 @@
+# GTAV-1HKO
+
+A one-hit KO mod for GTA V (singleplayer).
+
+* You to die if you take any damage.
+* Trying to activate Trevor's power will drain it immediately.
+
+## Requirements
+
+- [ScriptHookV](https://www.gta5-mods.com/tools/script-hook-v)
+
+## Installation
+
+Download and place `1HKO.asi` in your GTA V folder.
---
+++ script.cpp
@@ -0,0 +1,42 @@
+#include "script.h"
+
+Hash Trevor = 2608926626u;
+
+void ScriptMain() {
+	Ped playerPed = PLAYER::PLAYER_PED_ID();
+
+	Ped current = playerPed;
+	int health = ENTITY::GET_ENTITY_HEALTH(current);
+	int armor = PED::GET_PED_ARMOUR(current);
+
+	while (true) {
+		Player player = PLAYER::PLAYER_ID();
+		Ped newPed = PLAYER::GET_PLAYER_PED(player);
+
+		if (newPed != current) {
+			current = newPed;
+			health = ENTITY::GET_ENTITY_HEALTH(current);
+			armor = PED::GET_PED_ARMOUR(current);
+		}
+
+		if (PED::IS_PED_DEAD_OR_DYING(current, true)) {
+			WAIT(0);
+			continue;
+		}
+
+		if (PED::IS_PED_MODEL(current, Trevor) && PLAYER::IS_SPECIAL_ABILITY_ACTIVE(player)) {
+			PLAYER::SPECIAL_ABILITY_DEPLETE_METER(player, true);
+			WAIT(0);
+			continue;
+		}
+
+		int current_health = ENTITY::GET_ENTITY_HEALTH(current);
+		int current_armor = PED::GET_PED_ARMOUR(current);
+
+		if (current_health < health || current_armor < armor) {
+			ENTITY::SET_ENTITY_HEALTH(current, 0);
+		}
+
+		WAIT(0);
+	}
+}
