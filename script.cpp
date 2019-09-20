beb36ce221ff4b95aee41d60afec13d3fa18416c
John-John Tedro <udoprog@tedro.se>
2019-09-20 03:54:17 +0200

    Take care of switching ped and changes in max health

--- script.cpp
+++ script.cpp
@@ -3,46 +3,41 @@
 Hash Trevor = 2608926626u;
 
 void ScriptMain() {
-	Ped playerPed = PLAYER::PLAYER_PED_ID();
-
-	Ped current = playerPed;
-	int health = ENTITY::GET_ENTITY_HEALTH(current);
-	int armor = PED::GET_PED_ARMOUR(current);
-	bool just_died = false;
+	Ped current_ped = PLAYER::PLAYER_PED_ID();
+	int health = ENTITY::GET_ENTITY_HEALTH(current_ped);
+	int max_health = ENTITY::GET_ENTITY_MAX_HEALTH(current_ped);
 
 	while (true) {
 		Player player = PLAYER::PLAYER_ID();
-		Ped newPed = PLAYER::GET_PLAYER_PED(player);
+		Ped new_ped = PLAYER::GET_PLAYER_PED(player);
 
-		if (newPed != current) {
-			current = newPed;
-			health = ENTITY::GET_ENTITY_HEALTH(current);
-			armor = PED::GET_PED_ARMOUR(current);
+		int current_health = ENTITY::GET_ENTITY_HEALTH(current_ped);
+		int current_max_health = ENTITY::GET_ENTITY_MAX_HEALTH(current_ped);
+
+		// ped switches on death, so this must be before the death check below.
+		if (new_ped != current_ped) {
+			current_ped = new_ped;
+			health = current_health;
+			max_health = current_max_health;
 		}
 
-		if (PED::IS_PED_DEAD_OR_DYING(current, true)) {
-			just_died = true;
+		if (PED::IS_PED_DEAD_OR_DYING(current_ped, true)) {
 			WAIT(0);
 			continue;
 		}
 
-		if (just_died) {
-			health = ENTITY::GET_ENTITY_HEALTH(current);
-			armor = PED::GET_PED_ARMOUR(current);
-			just_died = false;
-		}
-
-		if (PED::IS_PED_MODEL(current, Trevor) && PLAYER::IS_SPECIAL_ABILITY_ACTIVE(player)) {
+		if (PED::IS_PED_MODEL(current_ped, Trevor) && PLAYER::IS_SPECIAL_ABILITY_ACTIVE(player)) {
 			PLAYER::SPECIAL_ABILITY_DEPLETE_METER(player, true);
-			WAIT(0);
-			continue;
 		}
 
-		int current_health = ENTITY::GET_ENTITY_HEALTH(current);
-		int current_armor = PED::GET_PED_ARMOUR(current);
+		// change current health if max health has changed.
+		if (max_health != current_max_health) {
+			health = current_health;
+			max_health = current_max_health;
+		}
 
-		if (current_health < health || current_armor < armor) {
-			ENTITY::SET_ENTITY_HEALTH(current, 0);
+		if (current_health < health) {
+			ENTITY::SET_ENTITY_HEALTH(current_ped, 0);
 		}
 
 		WAIT(0);
