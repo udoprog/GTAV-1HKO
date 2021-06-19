e242aedc6b281470220ddf8e09d4a323305c43bd
Rayope <84181546+Rayope@users.noreply.github.com>
2021-06-19 22:37:12 +0200

    User settings + code readability

--- script.cpp
+++ script.cpp
@@ -1,108 +1,112 @@
 #include "script.h"
 
+#include <mutex>
+#include <fstream>
+#include <sstream>
+#include <filesystem>
+
 const Hash Trevor = 2608926626u;
+const std::string settings_filename = "1HKO.ini";
+DisplaySettings disp_s;
+
 bool mod_active = true;
 bool mod_training = true;
 bool mod_needs_init = true;
 bool mod_manual = false;
+std::mutex hit_count_mutex;
 int hit_count = 0;
 
 bool control_key_pressed = false;
 
 
 void ScriptMain() {
-	Ped current_ped;
-	int max_health, health;
-	int max_armour, armour;
-	std::string mod_status;
-	std::string hit_count_str;
+	PedStats previous;
+	PedStats current;
+
+	// load/save settings
+	ReloadSettings();
+
+	std::string mod_status, hit_count_str;
 	bool trevor_ability_was_locked = false;
 
 	while (true) {
 		if (mod_active) {
+			Player player = PLAYER::PLAYER_ID();
+			Ped new_ped = PLAYER::PLAYER_PED_ID();
+
 			// initialize at start and if mod has been toggled on/off
 			if (mod_needs_init) {
-				current_ped = PLAYER::PLAYER_PED_ID();
-				health = ENTITY::GET_ENTITY_HEALTH(current_ped);
-				max_health = ENTITY::GET_ENTITY_MAX_HEALTH(current_ped);
-				armour = PED::GET_PED_ARMOUR(current_ped);
-				max_armour = PLAYER::GET_PLAYER_MAX_ARMOUR(PLAYER::PLAYER_ID());
+				std::lock_guard<std::mutex> lock(hit_count_mutex);
+				SetStatVarsFromPed(new_ped, player, previous);
 				mod_needs_init = false;
 				mod_manual = false;
 				hit_count = 0;
 			}
 
-			Player player = PLAYER::PLAYER_ID();
-			Ped new_ped = PLAYER::PLAYER_PED_ID();
+			// update current health and armour
+			SetStatVarsFromPed(new_ped, player, current);
 
 			// ped switches on death, so this must be before the death check below.
-			if (new_ped != current_ped) {
-				current_ped = new_ped;
-				health = ENTITY::GET_ENTITY_HEALTH(current_ped);
-				max_health = ENTITY::GET_ENTITY_MAX_HEALTH(current_ped);
-				armour = PED::GET_PED_ARMOUR(current_ped);
-				max_armour = PLAYER::GET_PLAYER_MAX_ARMOUR(player);
+			if (current.ped != previous.ped) {
+				SetStatVarsFromPed(current.ped, player, previous);
 			}
 
-			// update current health and armour
-			int current_health = ENTITY::GET_ENTITY_HEALTH(current_ped);
-			int current_max_health = ENTITY::GET_ENTITY_MAX_HEALTH(current_ped);
-			int current_armour = PED::GET_PED_ARMOUR(current_ped);
-			int current_max_armour = PLAYER::GET_PLAYER_MAX_ARMOUR(player);
-
 			// stop doing anything while dying
-			if (PED::IS_PED_DEAD_OR_DYING(current_ped, true)) {
+			if (PED::IS_PED_DEAD_OR_DYING(current.ped, true)) {
 				WAIT(0);
 				continue;
 			}
 
 			// disable Trevor special ability
-			if (PED::IS_PED_MODEL(current_ped, Trevor) && PLAYER::IS_SPECIAL_ABILITY_UNLOCKED(Trevor)) {
+			if (PED::IS_PED_MODEL(current.ped, Trevor) && PLAYER::IS_SPECIAL_ABILITY_UNLOCKED(Trevor)) {
 				PLAYER::SPECIAL_ABILITY_LOCK(Trevor);
 				trevor_ability_was_locked = true;
 			}
 
-			// change max health if it changed.
-			if (max_health != current_max_health) {
-				max_health = current_max_health;
-				// Check if max_health has been reduced
-				if (health > max_health)
-					health = max_health;
+
+			// change max health if it changed
+			if (previous.max_health != current.max_health) {
+				previous.max_health = current.max_health;
+				// check if max_health has been reduced
+				if (previous.health > previous.max_health)
+					previous.health = previous.max_health;
 			}
-
 			// change health if it increased
-			if (current_health > health)
-				health = current_health;
+			if (current.health > previous.health)
+				previous.health = current.health;
 
-			// change max armour if it changed.
-			if (max_armour != current_max_armour) {
-				max_armour = current_max_armour;
-				// Check if max_armour has been reduced
-				if (armour > max_armour)
-					armour = max_armour;
+			// change max armour if it changed
+			if (previous.max_armour != current.max_armour) {
+				previous.max_armour = current.max_armour;
+				// check if max_armour has been reduced
+				if (previous.armour > previous.max_armour)
+					previous.armour = previous.max_armour;
 			}
 			// change armour if it increased
-			if (current_armour > armour)
-				armour = current_armour;
+			if (current.armour > previous.armour)
+				previous.armour = current.armour;
 
+			
+			// health drop check
+			if (current.health < previous.health) {
+				std::lock_guard<std::mutex> lock(hit_count_mutex);
 
-			// Health drop check
-			if (current_health < health) {
-				// Kill the ped if not training
+				// kill the ped if not training
 				if (mod_training)
-					health = current_health;
+					previous.health = current.health;
 				else
-					ENTITY::SET_ENTITY_HEALTH(current_ped, 0);
+					ENTITY::SET_ENTITY_HEALTH(current.ped, 0);
 				hit_count++;
 			}
+			// armour drop check
+			if (current.armour < previous.armour) {
+				std::lock_guard<std::mutex> lock(hit_count_mutex);
 
-			// Armour drop check
-			if (current_armour < armour) {
-				// Kill the ped if not training
+				// kill the ped if not training
 				if (mod_training)
-					armour = current_armour;
+					previous.armour = current.armour;
 				else
-					ENTITY::SET_ENTITY_HEALTH(current_ped, 0);
+					ENTITY::SET_ENTITY_HEALTH(current.ped, 0);
 				hit_count++;
 			}
 		}
@@ -117,14 +121,30 @@ void ScriptMain() {
 			}
 		}
 
-		// Display on screen
+		// display on screen
 		mod_status = std::string("1HKO") + (mod_training ? " (train)" : "") + ": " + (mod_active ? "ON" : "OFF");
 		hit_count_str = std::string("Hits") + (mod_manual ? " (manual)" : "") + ": " + std::to_string(hit_count);
-		DrawTextToScreen(mod_status.c_str(), 0.9f, 0.5f, 0.3f, eFont::FontChaletLondon, false, 255, 255, 255);
-		DrawTextToScreen(hit_count_str.c_str(), 0.9f, 0.53f, 0.3f, eFont::FontChaletLondon, false, 255, 255, 255);
+		DrawTextToScreen(mod_status.c_str(), disp_s.offset_x, disp_s.offset_y, disp_s.size,
+			disp_s.font, false,
+			disp_s.red, disp_s.green, disp_s.blue);
+		DrawTextToScreen(hit_count_str.c_str(), disp_s.offset_x, disp_s.offset_y + 0.02f, disp_s.size,
+			disp_s.font, false,
+			disp_s.red, disp_s.green, disp_s.blue);
 
 		WAIT(0);
 	}
+}
+
+void SetStatVarsFromPed(int ped_id, int player_id, PedStats & stats) {
+	stats.ped = ped_id;
+
+	// health
+	stats.health = ENTITY::GET_ENTITY_HEALTH(ped_id);
+	stats.max_health = ENTITY::GET_ENTITY_MAX_HEALTH(ped_id);
+
+	// armour
+	stats.armour = PED::GET_PED_ARMOUR(ped_id);
+	stats.max_armour = PLAYER::GET_PLAYER_MAX_ARMOUR(player_id);
 }
 
 void OnKeyboardMessage(DWORD key, WORD repeats, BYTE scan_code, BOOL is_extended, BOOL is_with_alt, BOOL was_down_before, BOOL is_up_now) {
@@ -135,18 +155,23 @@ void OnKeyboardMessage(DWORD key, WORD repeats, BYTE scan_code, BOOL is_extended
 	else {
 		if (key == VK_CONTROL)
 			control_key_pressed = true;
-
-		// Decrease hit count
+		// decrease hit count
 		else if (control_key_pressed && key == 0x4A) { // Ctrl + Key J
 			if (hit_count > 0) {
+				std::lock_guard<std::mutex> lock(hit_count_mutex);
 				mod_manual = true;
 				hit_count--;
 			}
 		}
-		// Increase hit count
+		// increase hit count
 		else if (control_key_pressed && key == 0x4B) { // Ctrl + Key K
+			std::lock_guard<std::mutex> lock(hit_count_mutex);
 			mod_manual = true;
 			hit_count++;
+		}
+		// reload settings
+		else if (control_key_pressed && key == 0x4C) { // Ctrl + Key L
+			ReloadSettings();
 		}
 		// activate 1HKO
 		else if (key == 0x4B) { // Key K
@@ -156,7 +181,7 @@ void OnKeyboardMessage(DWORD key, WORD repeats, BYTE scan_code, BOOL is_extended
 				mod_needs_init = true;
 		}
 		// activate training mode
-		else if (key == 0x4A)
+		else if (key == 0x4A) // Key J
 			mod_training = !mod_training;
 	}
 }
@@ -174,4 +199,43 @@ void DrawTextToScreen(const std::string & text, float x, float y, float scale, e
 	UI::_SET_TEXT_ENTRY((char *) "STRING");
 	UI::_ADD_TEXT_COMPONENT_STRING3((char *) text.c_str());
 	UI::_DRAW_TEXT(x, y);
+}
+
+void ReloadSettings() {
+	if (std::filesystem::exists(settings_filename))
+		LoadSettings(settings_filename, disp_s);
+	else
+		SaveSettings(settings_filename, disp_s);
+}
+
+void LoadSettings(const std::string & filename, DisplaySettings & s) {
+	std::ifstream file(filename);
+	std::string line;
+
+	while (std::getline(file, line)) {
+		std::istringstream current(line.substr(line.find("=") + 1));
+
+		if (line.find("offset_x") != -1)
+			current >> s.offset_x;
+		else if (line.find("offset_y") != -1)
+			current >> s.offset_y;
+		else if (line.find("size") != -1)
+			current >> s.size;
+		else if (line.find("red") != -1)
+			current >> s.red;
+		else if (line.find("green") != -1)
+			current >> s.green;
+		else if (line.find("blue") != -1)
+			current >> s.blue;
+	}
+}
+
+void SaveSettings(const std::string & filename, const DisplaySettings & s) {
+	std::ofstream file(filename);
+	file << "offset_x = " << s.offset_x << std::endl;
+	file << "offset_y = " << s.offset_y << std::endl;
+	file << "size = " << s.size << std::endl;
+	file << "red = " << s.red << std::endl;
+	file << "green = " << s.green << std::endl;
+	file << "blue = " << s.blue << std::endl;
 }
--- script.h
+++ script.h
@@ -8,6 +8,31 @@
 
 #include <string>
 
+struct PedStats {
+	int ped;
+	int health;
+	int max_health;
+	int armour;
+	int max_armour;
+};
+
+struct DisplaySettings {
+	float offset_x = 0.9f;
+	float offset_y = 0.55f;
+	float size = 0.3f;
+	eFont font = eFont::FontChaletLondon;
+	int red = 255;
+	int green = 255;
+	int blue = 255;
+};
+
 void ScriptMain();
+
+void SetStatVarsFromPed(int ped_id, int player_id, PedStats & stats);
+
 void OnKeyboardMessage(DWORD key, WORD repeats, BYTE scan_code, BOOL is_extended, BOOL is_with_alt, BOOL was_down_before, BOOL is_up_now);
 void DrawTextToScreen(const std::string & text, float x, float y, float scale, eFont font, bool align_right, int red, int green, int blue);
+
+void ReloadSettings();
+void LoadSettings(const std::string & filename, DisplaySettings & s);
+void SaveSettings(const std::string & filename, const DisplaySettings & s);
